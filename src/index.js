ff7884c5ae0b21064acb984e841f2ce198f317cb
Asmita Singh <asmita.singh@logipe.com>
2023-03-12 21:48:26 +0530

    adding gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 require('../src/db/mongoose')
 const user_router= require('./routers/user_router');
-const task_router= require('./routers/task_roouter')
+const task_router= require('./routers/task_router')
 
 const app = express();
 const port = process.env.PORT || 3000
